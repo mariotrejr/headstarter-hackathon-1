ddc9c7463b6361ce1fdfdef9e7af522a8e22f450
mariotrejr <mario.a.trevinojr@gmail.com>
2024-08-01 00:06:16 -0500

    almmost done

--- agoraClient.js
+++ agoraClient.js
@@ -1,85 +1,88 @@
+import { createClient, createScreenVideoTrack } from 'agora-rtc-sdk-ng';
+
 let client;
 let isClientInitialized = false;
 
-const initAgoraClient = async () => {
-  if (typeof window !== 'undefined') {
-    const AgoraRTC = await import('agora-rtc-sdk-ng');
-    client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+const initAgoraClient = async (appId, channel, token) => {
+  client = createClient({ mode: 'rtc', codec: 'vp8' });
 
-    client.on('user-published', async (user, mediaType) => {
-      try {
-        console.log('User published:', user.uid, 'Media type:', mediaType);
-        await client.subscribe(user, mediaType);
-        console.log('Subscribed to user:', user.uid);
+  client.on('user-published', async (user, mediaType) => {
+    await handleUserPublished(user, mediaType);
+  });
 
-        if (mediaType === 'video') {
-          const remoteVideoTrack = user.videoTrack;
-          const remoteVideoContainer = document.createElement('div');
-          remoteVideoContainer.id = `remote-user-${user.uid}`;
-          remoteVideoContainer.style.width = '100%';
-          remoteVideoContainer.style.height = '100%';
-          document.getElementById('remote-screen-share-container').appendChild(remoteVideoContainer);
-          remoteVideoTrack.play(remoteVideoContainer.id);
-        }
+  client.on('user-unpublished', handleUserUnpublished);
 
-        if (mediaType === 'audio') {
-          const remoteAudioTrack = user.audioTrack;
-          remoteAudioTrack.play();
-        }
-      } catch (error) {
-        console.error('Failed to subscribe to user:', error);
-      }
-    });
-
-    client.on('user-unpublished', user => {
-      console.log('User unpublished:', user.uid);
-      const remoteVideoContainer = document.getElementById(`remote-user-${user.uid}`);
-      if (remoteVideoContainer) {
-        remoteVideoContainer.remove();
-      }
-    });
-
-    try {
-      await client.join('65f5d5d8718244e6acec2f20a19b2278', 'mainRoom', null, null);
-      console.log('Successfully joined the Agora channel');
-      isClientInitialized = true;
-    } catch (error) {
-      console.error('Error joining Agora channel:', error);
-    }
+  try {
+    await client.join(appId, channel, token, null);
+    console.log('Successfully joined the Agora channel');
+    isClientInitialized = true;
+  } catch (error) {
+    console.error('Error joining Agora channel:', error);
   }
+
   return client;
 };
 
+const handleUserPublished = async (user, mediaType) => {
+  try {
+    console.log('User published:', user.uid, 'Media type:', mediaType);
+    await client.subscribe(user, mediaType);
+    console.log('Subscribed to user:', user.uid);
+
+    if (mediaType === 'video') {
+      const remoteVideoTrack = user.videoTrack;
+      const remoteVideoContainer = document.createElement('div');
+      remoteVideoContainer.id = `remote-user-${user.uid}`;
+      remoteVideoContainer.style.width = '100%';
+      remoteVideoContainer.style.height = '100%';
+      document.getElementById('remote-screen-share-container').appendChild(remoteVideoContainer);
+      remoteVideoTrack.play(remoteVideoContainer.id);
+    }
+
+    if (mediaType === 'audio') {
+      const remoteAudioTrack = user.audioTrack;
+      remoteAudioTrack.play();
+    }
+  } catch (error) {
+    console.error('Failed to subscribe to user:', error);
+  }
+};
+
+const handleUserUnpublished = (user) => {
+  console.log('User unpublished:', user.uid);
+  const remoteVideoContainer = document.getElementById(`remote-user-${user.uid}`);
+  if (remoteVideoContainer) {
+    remoteVideoContainer.remove();
+  }
+};
+
 const startScreenSharing = async (videoElementId) => {
-  if (typeof window !== 'undefined') {
-    if (!isClientInitialized) {
-      console.error('Agora client is not initialized');
-      return;
-    }
+  if (!isClientInitialized) {
+    console.error('Agora client is not initialized');
+    return;
+  }
 
-    const AgoraRTC = await import('agora-rtc-sdk-ng');
-    const localScreenTrack = await AgoraRTC.createScreenVideoTrack({
-      encoderConfig: '1080p_1',
-    });
-
-    if (client) {
-      try {
-        await client.publish(localScreenTrack);
-        localScreenTrack.play(videoElementId, { fit: 'contain' });
-        console.log('Screen sharing started and track is playing in element:', videoElementId);
-      } catch (error) {
-        console.error('Error publishing screen track:', error);
-      }
-    } else {
-      console.error('Agora client is not initialized');
-    }
+  try {
+    const [localScreenTrack] = await createScreenVideoTrack({ encoderConfig: '1080p_1' });
+    await client.publish(localScreenTrack);
+    localScreenTrack.play(videoElementId, { fit: 'contain' });
+    console.log('Screen sharing started and track is playing in element:', videoElementId);
+  } catch (error) {
+    console.error('Error publishing screen track:', error);
   }
 };
 
 const stopScreenSharing = async () => {
-  if (typeof window !== 'undefined') {
-    // Logic to stop screen sharing can be added here
+  if (!isClientInitialized) {
+    console.error('Agora client is not initialized');
+    return;
+  }
+
+  try {
+    await client.unpublish(client.localTracks.filter(track => track.trackId.includes('scr')));
     console.log('Screen sharing stopped');
+  } catch (error) {
+    console.error('Error stopping screen sharing:', error);
   }
 };
 
